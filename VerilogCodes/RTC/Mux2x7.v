99e2327a1d7f392a6a652b0f36984ff612ceb1b2
Kmena <kmena281@gmail.com>
2016-09-19 10:50:22 -0600

    cracion del mux 2 x 7

--- VerilogCodes/RTC/Mux2x7.v
+++ VerilogCodes/RTC/Mux2x7.v
@@ -18,8 +18,18 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module Mux2x7(
-    );
+module Mux2x7(Dato1,Dato2,selector,salida);
+	input selector;
+	input [7:0] Dato1,Dato2;
+	output salida;
+	reg [7:0]salida;
+	//assign salida=(selector)?Dato1:(selector2)?Dato2:Dato3;
+	always @ (Dato1 or Dato2 or selector)
+	begin
+		if(selector) salida=Dato1;
+		else salida=Dato2;
+	end
 
 
 endmodule
+
