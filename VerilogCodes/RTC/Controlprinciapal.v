2a35793b42bdc8f9ecffe5a0b136ae9079a7cada
Kmena <kmena281@gmail.com>
2016-09-20 10:31:49 -0600

    actualizacion del control principal
    eliminacion de estados innecesarios

--- VerilogCodes/RTC/Controlprinciapal.v
+++ VerilogCodes/RTC/Controlprinciapal.v
@@ -1,3 +1,23 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    10:12:55 09/15/2016 
+// Design Name: 
+// Module Name:    controlprinciapal 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
 module controlprinciapal(reset,CLK,finint,finwt,finct,usuario,iniciar,whileT,CrontUs);
 	input reset,CLK,finint,finwt,finct,usuario;
 	output iniciar,whileT,CrontUs;
@@ -6,12 +26,10 @@ module controlprinciapal(reset,CLK,finint,finwt,finct,usuario,iniciar,whileT,Cro
 	reg [2:0] NextState;
 	//estados
 	
-	parameter [2:0] inicializar = 3'b000;
-	parameter [2:0] Whiletrue = 3'b001;
-	parameter [2:0] actualizacion1 = 3'b010;
-	parameter [2:0] solicitud = 3'b011;
-	parameter [2:0] actualizacion2 = 3'b100;
-	parameter [2:0] controlusuario = 3'b101;
+	parameter [2:0] inicializar = 2'b00;
+	parameter [2:0] Whiletrue = 2'b01;
+	parameter [2:0] solicitud = 2'b10;
+	parameter [2:0] controlusuario = 2'b11;
 	
 	always@ (finint or finwt or finct or usuario or State)
 	begin
@@ -21,15 +39,11 @@ module controlprinciapal(reset,CLK,finint,finwt,finct,usuario,iniciar,whileT,Cro
 		if(finint)NextState=Whiletrue;
 		else NextState=inicializar;
 	Whiletrue:
-		if(finwt)NextState=actualizacion1;
+		if(finwt)NextState=solicitud;
 		else NextState=Whiletrue;
-	actualizacion1:
-		NextState=solicitud;
 	solicitud:
-		if(usuario)NextState=actualizacion2;
+		if(usuario)NextState=controlusuario;
 		else NextState=Whiletrue;
-	actualizacion2:
-		NextState=controlusuario;
 	controlusuario:
 		if(finct)NextState=Whiletrue;
 		else NextState=controlusuario;
@@ -51,19 +65,21 @@ module controlprinciapal(reset,CLK,finint,finwt,finct,usuario,iniciar,whileT,Cro
 			State<=NextState;
 			case(State)
 				inicializar:
+				begin
 					iniciar<=1;
+					whileT<=0;
+					CrontUs<=0;
+				end
 				Whiletrue:
 					begin
 						CrontUs<=0;
 						iniciar<=0;
 						whileT<=1;
 					end
-				actualizacion1:
-					begin
-						whileT<=0;
-					end
 				controlusuario:
 				begin
+					iniciar<=0;
+					whileT<=0;
 					CrontUs<=1;
 				end
 				default:
